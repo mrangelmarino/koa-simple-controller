aa9505c8ce51961c644d8f1ae4260a6872dc6a2f
Angel Marino <me@angelmarino.com>
2015-07-06 00:47:15 -0400

    index.js controller object builder and initalizer functions

---
+++ index.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var async = require('async');
+var path = require('path');
+
+// Create new Controller object and specify fallback directory
+function Controller (directory){
+
+    // Looks in default "controller" directory at root
+    this.directory = directory || './controller';
+
+    if (!(this instanceof Controller)) {
+        return new Controller(directory);
+    }
+
+};
+
+// Return promise consisting of object built from contents of files
+Controller.prototype.buildControllers = function(){
+
+    return new Promise(function (resolve, reject) {
+
+        var directory = this.directory;
+
+        var controllers = {};
+
+        fs.readdir(directory, function (err, files) {
+
+            if (err) return reject(err);
+
+            async.each(files, function(item, callback){
+                var controllerName = path.basename(item, '.js');
+                controllers[controllerName] = require('../.'+directory+'/'+item);
+                callback();
+            }, function(err){
+                if (err) reject(err);
+                resolve(controllers);
+            });
+
+        });
+
+    });
+
+};
+
+// Calls buildControllers to build controller object
+// and to assign object to the Koa context and to the controller object
+Controller.prototype.initialize = function(){
+    var Controller = this;
+    return function* (next){
+        ctx = this;
+        ctx.controllers = Controller.ctrl = yield Controller.buildControllers();
+        yield* next;
+    };
+};
+
+module.exports = Controller;
